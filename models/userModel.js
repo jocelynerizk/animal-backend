de4c96778a08f8b6facb8a0ec4d31b4d1c8c2ddd
jocelynerizk <95102544+jocelynerizk@users.noreply.github.com>
2024-01-21 23:13:03 +0200

    hosting backend

--- models/userModel.js
+++ models/userModel.js
@@ -1,21 +1,24 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const userSchema = new Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  // verfied:{type:Boolean ,default:false}
-  phoneNumber: { type: Number, required: true, unique: true },
-  role: {
-    type: String,
-    enum: ["admin", "employee", "client"],
-    required: true,
-    default: "client",
+const userSchema = new Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    // verfied:{type:Boolean ,default:false}
+    phoneNumber: { type: Number, required: true, unique: true },
+    role: {
+      type: String,
+      enum: ["admin", "employee", "client"],
+      required: true,
+      default: "client",
+    },
+    firebaseUid: { type: String },
   },
-},
-  { timestamps: true });
+  { timestamps: true }
+);
 const user = model("user", userSchema);
 
 module.exports = user;
